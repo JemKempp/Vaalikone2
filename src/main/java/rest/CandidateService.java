8bc592b118803d2c208f36456ff2f0046083bc29
kirsi99 <kirsi.koskinen@student.hamk.fi>
2022-04-28 13:49:21 +0300

    edited all the files

--- src/main/java/rest/CandidateService.java
+++ src/main/java/rest/CandidateService.java
@@ -1,42 +1,63 @@
-package services;
+package rest;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import data.Candidates;
 
 
 @Path("/CandidateService")
+
 public class CandidateService {
-	
-	
-	@GET
-	@Path("/servicename")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String serviceName() {
-		return "";
-	}
-	
+@Context
+HttpServletRequest request;
+@Context
+HttpServletResponse response;
+
 	
 	@GET
-	@Path("//{}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getOneLaptop(@PathParam("ehdokas_id") int id) {
-		String s=getCandidate(id);
-		return s;
-	}
-	
-	
-	public String getCandidate(int id) {
-		ArrayList list=new ArrayList<>();
-		list.add("");
-		list.add("");
-		list.add("");
-		list.add("");
-		list.add("");
+	@Path("/getCandidates")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void getCandidates() {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("vaalikone2");
+		//And then EntityManager, which can manage the entities.
+		EntityManager em=emf.createEntityManager();
 		
-		return list.get(id);
+		//Read all the rows from table prey. Here the Prey must start with capital, 
+		//because class's name starts. This returns a List of Prey objects.
+		List<Candidates> list=em.createQuery("select a from Candidates a").getResultList();
+		//return list;
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/showcandidates.jsp");
+		request.setAttribute("ehdokaslista", list);
+        try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	
+//	@GET
+//	@Path("//{}")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public String getOneLaptop(@PathParam("ehdokas_id") int id) {
+//		String s=getCandidate(id);
+//		return s;
+//	}
 }
